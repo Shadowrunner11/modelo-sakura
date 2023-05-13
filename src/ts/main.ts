c2d966585bc3b2beda78648fca76aacc5afceec3
shadowrunner11 <pieroramirez810@gmail.com>
2023-05-13 13:35:09 -0500

    :lipstick: created new config

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,27 +1,23 @@
-import ScrollReveal from "scrollreveal";
+import scrollRevealBuilder from "scrollreveal";
+
+const ScrollReveal = scrollRevealBuilder({reset: true});
 
 
-const animFlower = ScrollReveal();
-
-animFlower.reveal('.anim-flower', {
+ScrollReveal.reveal('.anim-flower', {
   duration: 3000,
   delay: 0,
   distance: '150px',
   opacity: 0,
   easing: 'ease-in-out',
   origin: 'bottom',
-  reset: true
 });
 
-const anim2 = ScrollReveal();
-
-anim2.reveal('.anim-2', {
+ScrollReveal.reveal('.anim-2', {
   duration: 1500,
   delay: 0,
   opacity: 0,
   easing: 'ease-in-out',
-  reset: true,
-  origin: 'left',
+  origin: 'bottom',
   distance: '50px',
   rotate: { x: 0, y: 0, z: 40 },
   scale: 0.5,
