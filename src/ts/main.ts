735f1401a400b2de43b01be25d1f4c634a1e3b4b
shadowrunner11 <pieroramirez810@gmail.com>
2023-05-13 13:38:54 -0500

    :lipstick: added ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -22,3 +22,24 @@ ScrollReveal.reveal('.anim-2', {
   rotate: { x: 0, y: 0, z: 40 },
   scale: 0.5,
 });
+
+ScrollReveal.reveal('.anim-3', {
+  duration: 1000,
+  delay: 0,
+  opacity: 0,
+  easing: 'ease-in-out',
+  origin: 'left',
+  distance: '0px',
+  scale: 0.5,
+});
+
+
+ScrollReveal.reveal('.anim-4', {
+  duration: 1000,
+  delay: 0,
+  easing: 'ease-in-out',
+  origin: 'bottom',
+  distance: '100px',
+  opacity: 0,
+  scale: 0.5,
+});
